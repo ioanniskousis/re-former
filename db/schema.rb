efec15b933aa0ff3b3b7bc8b52184d8a87782d83
ericmbouwe <ericmbouwe@yahoo.fr>
2020-06-06 18:26:04 +0100

    code formated

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_06_04_084419) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username",  null: false
-    t.string "email",     null: false
-    t.string "password",  null: false
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
